3ffb5f26b1471aeeb3136db69c1ee0d05a8e1733
JackAtlas <jackatlas8864@gmail.com>
2025-04-01 16:34:52 +0800

    clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: 'ChiForm',
@@ -12,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="zh-CN" suppressHydrationWarning>
-      <body className="antialiased">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="zh-CN" suppressHydrationWarning>
+        <body className="antialiased">{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
