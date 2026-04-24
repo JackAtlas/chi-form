0dcd4b0ba29582b74524bf7ca0055765947cb3fa
JackAtlas <jackatlas8864@gmail.com>
2026-04-24 20:13:27 +0800

    fix: sign up page is public

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,10 @@ import {
   createRouteMatcher
 } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)'
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
