a4c0c77f14643245dbd4b109f3abefa62a01c88a
JackAtlas <jackatlas8864@gmail.com>
2026-04-25 20:43:10 +0800

    fix: type inference `required: z.boolean().default(false)` will be `required?: boolean`, but form expect `required: boolean`

--- components/fields/DateField.tsx
+++ components/fields/DateField.tsx
@@ -44,7 +44,7 @@ const extraAttributes = {
 const propertiesSchema = z.object({
   label: z.string().min(2).max(50),
   helperText: z.string().max(200),
-  required: z.boolean().default(false)
+  required: z.boolean()
 })
 
 export const DateFieldFormElement: FormElement = {
@@ -200,7 +200,7 @@ function PropertiesComponent({
     defaultValues: {
       label,
       helperText,
-      required
+      required: required ?? false
     }
   })
 
